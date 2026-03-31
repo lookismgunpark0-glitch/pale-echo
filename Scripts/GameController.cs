83cde6e03f9a12c0f230fdc10614b4ed4307f28f
lookismgunpark0-glitch <lookismgunpark0@gmail.com>
2026-03-31 02:36:50 +0200

    Add GameController script

---
+++ Scripts/GameController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour 
+{
+    void Start()
+    {
+        // Initialize game settings
+    }
+
+    void Update()
+    {
+        // Handle game logic
+    }
+}
